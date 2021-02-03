ad564e6ff84452e7a95ffa929cb819499bb796ea
aravindd7 <ak154761@gmail.com>
2021-02-03 09:33:41 +0530

    init

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import {Provider} from 'react-redux';
+
+import AppNavigator from './src/navigation/AppNavigator';
+import store from './src/redux/store';
+
+const loadFonts = () => {
+  return Font.loadAsync({
+    'Ubuntu': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'Ubuntu-Bold': require('./assets/fonts/Ubuntu-Bold.ttf')
+  });
+}
+
+export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={loadFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+});
